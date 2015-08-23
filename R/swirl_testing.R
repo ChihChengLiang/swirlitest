b7e7d21f32d9976007203d752e7d6322f1f972d0
ChihChengLiang <chihchengliang@gmail.com>
2015-08-24 00:24:27 +0800

    replace paste0 with file path

--- R/swirl_testing.R
+++ R/swirl_testing.R
@@ -5,9 +5,9 @@ test_lesson <- function(lesson_dir) {
     e <- new.env()
     # Since the initLesson might change working directory,
     # load lesson yaml first before that happens.
-    lesson <- yaml::yaml.load_file(paste0(lesson_dir, "lesson.yaml"))
+    lesson <- yaml::yaml.load_file(file.path(lesson_dir, "lesson.yaml"))
     for (R_file in c("customTests.R", "initLesson.R")) {
-        R_file_path <- paste0(lesson_dir, R_file)
+        R_file_path <- file.path(lesson_dir, R_file)
         if (file.exists(R_file_path))
             source(R_file_path, local = e)
     }
